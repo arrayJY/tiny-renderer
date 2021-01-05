46279339e5cc1aa70ac209e20d5033a2df8639fc
arrayJY <ji957455952@163.com>
2021-01-05 23:55:13 +0800

    ✨ Matrix getter/setter.

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,3 +1,6 @@
+use std::usize;
+
+use generic_array::typenum::Unsigned;
 use generic_array::typenum::{U1, U2, U3, U4};
 use generic_array::{ArrayLength, GenericArray};
 
@@ -10,6 +13,8 @@ where
 {
     data: GenericArray<GenericArray<T, Col>, Row>,
 }
+
+#[allow(dead_code)]
 impl<T, Row, Col> Matrix<T, Row, Col>
 where
     T: Default,
@@ -19,6 +24,55 @@ where
     pub fn new() -> Matrix<T, Row, Col> {
         Matrix::<T, Row, Col> {
             data: GenericArray::default(),
+        }
+    }
+
+    pub fn rows(&self) -> usize {
+        <Row as Unsigned>::to_u64() as usize
+    }
+
+    pub fn cols(&self) -> usize {
+        <Col as Unsigned>::to_u64() as usize
+    }
+
+    pub fn get(&self, row: usize, col: usize) -> Option<&T> {
+        if row < self.rows() && col < self.cols() {
+            Some(&self.data[row][col])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_mut(&mut self, row: usize, col: usize) -> Option<&T> {
+        if row < self.rows() && col < self.cols() {
+            Some(&self.data[row][col])
+        } else {
+            None
+        }
+    }
+
+    pub fn set(&mut self, row: usize, col: usize, value: T) -> Result<(), ()> {
+        if row < self.rows() && col < self.cols() {
+            self.data[row][col] = value;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+
+    pub fn get_col(&self, col: usize) -> Option<impl Iterator<Item = &T>> {
+        if col < self.cols() {
+            Some((0..self.rows()).map(move |row| self.get(row, col).unwrap()))
+        } else {
+            None
+        }
+    }
+
+    pub fn get_row(&self, row: usize) -> Option<impl Iterator<Item = &T>> {
+        if row < self.rows() {
+            Some((0..self.cols()).map(move |col| self.get(row, col).unwrap()))
+        } else {
+            None
         }
     }
 }
@@ -38,10 +92,10 @@ def_square_matrix!(Matrix4, U4);
 
 #[macro_export]
 macro_rules! matrix {
-    ($type: ty; $length: ty) => (
+    ($type: ty; $length: ty) => {
         Matrix::<$type, $length, $length>::new();
-    );
-    ($type: ty; $row: ty, $col: ty ) => (
+    };
+    ($type: ty; $row: ty, $col: ty ) => {
         Matrix::<$type, $row, $col>::new();
-    );
+    };
 }
