7286e0187f7043ba40917266177a6f58181256e1
arrayJY <ji957455952@163.com>
2021-01-29 00:29:38 +0800

    ✨ Vector creation macro.

--- src/algebra/vector.rs
+++ src/algebra/vector.rs
@@ -16,13 +16,53 @@ def_dimension_vector!(Vector2, U2);
 def_dimension_vector!(Vector3, U3);
 def_dimension_vector!(Vector4, U4);
 
-macro_rules! def_float_dimension_vector {
-    ($name: ident, $dimension: ty) => {
-        #[allow(dead_code)]
-        pub type $name = Vector<f64, $dimension>;
+#[allow(dead_code)]
+pub type Vectorf<N> = Vector<f64, N>;
+#[allow(dead_code)]
+pub type Vector1f = Vectorf<U1>;
+#[allow(dead_code)]
+pub type Vector2f = Vectorf<U2>;
+#[allow(dead_code)]
+pub type Vector3f = Vectorf<U3>;
+#[allow(dead_code)]
+pub type Vector4f = Vectorf<U4>;
+
+#[macro_export]
+macro_rules! vector {
+    ($type: ty, $dimension: ty) => {
+        Vector::<$type, $dimension>::new()
+    };
+    ($type: ty; $dimension : ty; $($val: expr), * ) => {
+        {
+        let mut v = Vector::<$type, $dimension>::new();
+        let mut iter = (0..v.rows());
+        $(
+            {
+                let i = iter.next().unwrap();
+                v[i][0] = $val;
+            }
+        )*
+        v
+        }
     };
 }
-def_float_dimension_vector!(Vector1f, U1);
-def_float_dimension_vector!(Vector2f, U2);
-def_float_dimension_vector!(Vector3f, U3);
-def_float_dimension_vector!(Vector4f, U4);
+
+#[macro_export]
+macro_rules! vectorf {
+    ($dimension: ty) => {
+        Vectorf::<$dimension>::new()
+    };
+    ($dimension : ty; $($val: expr), * ) => {
+        {
+        let mut v = Vectorf::<$dimension>::new();
+        let mut iter = 0..v.rows();
+        $(
+            {
+                let i = iter.next().unwrap();
+                v[i][0] = $val;
+            }
+        )*
+        v
+        }
+    };
+}
