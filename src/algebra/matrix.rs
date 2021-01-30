73392fe09a924e649fdebad2d10a4ebc7d0ebaa1
arrayJY <ji957455952@163.com>
2021-01-31 03:59:30 +0800

    ✨ Transformation matrix.

--- src/algebra/matrix.rs
+++ src/algebra/matrix.rs
@@ -259,3 +259,57 @@ macro_rules! matrixf {
        }
     };
 }
+
+#[allow(dead_code)]
+impl Matrix4f {
+    pub fn translation_matrix(x: f32, y: f32, z: f32) -> Matrix4f {
+        matrixf!(U4;
+            1.0, 0.0, 0.0,   x,
+            0.0, 1.0, 0.0,   y,
+            0.0, 0.0, 1.0,   z,
+            0.0, 0.0, 0.0, 1.0
+        )
+    }
+
+    pub fn scale_matrix(sx: f32, sy: f32, sz: f32) -> Matrix4f {
+        matrixf!(U4;
+             sx, 0.0, 0.0, 0.0,
+            0.0,  sy, 0.0, 0.0,
+            0.0, 0.0,  sz, 0.0,
+            0.0, 0.0, 0.0, 1.0
+        )
+    }
+
+    pub fn rotate_around_x_matrix(angle: f32) -> Matrix4f {
+        let sina = angle.sin();
+        let cosa = angle.cos();
+        matrixf!(U4;
+            1.0,  0.0,   0.0, 0.0,
+            0.0, cosa, -sina, 0.0,
+            0.0, sina,  cosa, 0.0,
+            0.0,  0.0,   0.0, 1.0
+        )
+    }
+
+    pub fn rotate_around_y_matrix(angle: f32) -> Matrix4f {
+        let sina = angle.sin();
+        let cosa = angle.cos();
+        matrixf!(U4;
+             cosa, 0.0, sina, 0.0,
+              0.0, 1.0,  0.0, 0.0,
+            -sina, 0.0, cosa, 0.0,
+              0.0, 0.0,  0.0, 1.0
+        )
+    }
+
+    pub fn rotate_around_z_matrix(angle: f32) -> Matrix4f {
+        let sina = angle.sin();
+        let cosa = angle.cos();
+        matrixf!(U4;
+             cosa, sina, 0.0, 0.0,
+             sina, cosa, 0.0, 0.0,
+              0.0,  0.0, 1.0, 0.0,
+              0.0,  0.0, 0.0, 1.0
+        )
+    }
+}
