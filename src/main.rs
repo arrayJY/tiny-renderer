b93e8047dacfa70fd0972f5a0f71a210cf5b4dfd
arrayJY <ji957455952@163.com>
2021-02-16 04:57:34 +0800

    ✨ A test.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 mod algebra;
 mod pipeline;
+mod renderer;
 mod window;
 
 #[cfg(test)]
 mod test;
 
-use window::Window;
-fn main() {
-    Window::new().run()
+use std::f32::consts::PI;
 
+use algebra::vector::Vector3f;
+use pipeline::{camera::Camera, model::Model};
+use renderer::Renderer;
+fn main() {
+    const WIDTH: usize = 800;
+    const HEIGHT: usize = WIDTH;
+    let model = Model::from_obj("box.obj").remove(0);
+    let camera = Camera::new()
+        .eye_position(vector3f!(5.0, 5.0, 5.0))
+        .gaze_direct(vector3f!(-1.0, -1.0, -1.0))
+        .up_direct(vector3f!(-1.0, 1.0, -1.0))
+        .eye_fov(PI / 2.0)
+        .aspect_ratio(WIDTH as f32 / HEIGHT as f32)
+        .near(1.0)
+        .far(100.0);
+
+    Renderer::new(WIDTH, HEIGHT)
+        .model(model)
+        .camera(camera)
+        .render();
 }
