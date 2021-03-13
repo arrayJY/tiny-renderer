d05c5713558f6d38831f17a80e9605054e7016c7
arrayJY <ji957455952@163.com>
2021-03-13 15:09:07 +0800

    🩹 Deepen z shader color.

--- src/pipeline/fragment_shader/z_shader.rs
+++ src/pipeline/fragment_shader/z_shader.rs
@@ -9,7 +9,7 @@ pub struct ZShader;
 #[allow(unused_variables)]
 impl FragmentShader for ZShader {
     fn shade(&self, triangle: &Triangle, barycenter: (f32, f32, f32), z: f32) -> Color {
-        &Color::rgb(255, 255, 255) * z
+        &Color::rgb(255, 255, 255) * (z * 5.0 - 4.0)
     }
     fn update_camera(&mut self, _: &Camera) {}
     fn update_light(&mut self, _: &Light) {}
